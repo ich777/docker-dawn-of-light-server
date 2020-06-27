34de41331746e05b70659f6ad9d69cc5998d41dd
ich777 <ich777@noreply.example.org>
2020-06-27 10:27:06 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,58 @@
 #!/bin/bash
-echo "---Sleep zZz---"
-sleep infinity
+LAT_V="$(curl -s https://api.github.com/repos/Dawn-of-Light/DOLSharp/releases | grep 'tag_name' | head -1 | cut -d '"' -f4)"
+CUR_V="$(find ${DATA_DIR} -name DoLv* | cut -d 'v' -f2)"
+
+echo "---Version Check---"
+if [ ! -f "${DATA_DIR}/DOLServer.exe" ]; then
+   	echo "---Dawn of Light Server not found, downloading!---"
+   	cd ${DATA_DIR}
+   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O DoL-$LAT_V.zip "https://github.com/Dawn-of-Light/DOLSharp/releases/download/$LAT_V/DOLServer_linux_net45_Release.zip" ; then
+		echo "---Successfully downloaded Dawn of Light Server v$LAT_V---"
+	else
+		echo "-----------------------------------------------------------------------------------------------------"
+		echo "------------Can't download Dawn of Light Server v$LAT_V, putting server into sleep mode-----------"
+		echo "-----------------------------------------------------------------------------------------------------"
+	fi
+    unzip -q ${DATA_DIR}/DoL-$LAT_V.zip
+    rm ${DATA_DIR}/DoL-$LAT_V.zip
+    touch ${DATA_DIR}/DoLv$LAT_V
+elif [ "$LAT_V" != "$CUR_V" ]; then
+    echo "---Newer version found, installing!---"
+    cd ${DATA_DIR}
+    mkdir ${DATA_DIR}/userbackup
+    cp -R ${DATA_DIR}/config ${DATA_DIR}/userbackup/
+    cp -R ${DATA_DIR}/logs ${DATA_DIR}/userbackup/
+    cp -R ${DATA_DIR}/scripts ${DATA_DIR}/userbackup/
+    cp ${DATA_DIR}/dol.sqlite* ${DATA_DIR}/userbackup/
+    find . -maxdepth 1 -not -name 'userbackup' -print0 | xargs -0 -I {} rm -R {} 2&>/dev/null
+    if wget -q -nc --show-progress --progress=bar:force:noscroll -O DoL-$LAT_V.zip "https://github.com/Dawn-of-Light/DOLSharp/releases/download/$LAT_V/DOLServer_linux_net45_Release.zip" ; then
+		echo "---Successfully downloaded Dawn of Light Server v$LAT_V---"
+	else
+		echo "-----------------------------------------------------------------------------------------------------"
+		echo "------------Can't download Dawn of Light Server v$LAT_V, putting server into sleep mode-----------"
+		echo "-----------------------------------------------------------------------------------------------------"
+	fi
+    unzip -q ${DATA_DIR}/DoL-$LAT_V.zip
+    rm ${DATA_DIR}/DoL-$LAT_V.zip
+    touch ${DATA_DIR}/DoLv$LAT_V
+    cp -R ${DATA_DIR}/userbackup/* ${DATA_DIR}
+    rm -R ${DATA_DIR}/userbackup
+elif [ "$LAT_V" == "$CUR_V" ]; then
+    echo "---Dawn of Light Server Version up-to-date---"
+else
+ 	echo "---Something went wrong, putting server in sleep mode---"
+ 	sleep infinity
+fi
+
+echo "---Preparing Server---"
+if [ ! -f "${DATA_DIR}/config/serverconfig.xml" ]; then
+    echo "---No server configuration found, creating template---"
+    if [ ! -d "${DATA_DIR}/config" ]; then
+        mkdir -p ${DATA_DIR}/config
+    fi
+    cp /tmp/serverconfig.xml ${DATA_DIR}/config/serverconfig.xml
+fi
+chmod -R ${DATA_PERM} ${DATA_DIR}
+
+cd ${DATA_DIR}
+mono ${DATA_DIR}/DOLServer.exe ${GAME_PARAMS}
